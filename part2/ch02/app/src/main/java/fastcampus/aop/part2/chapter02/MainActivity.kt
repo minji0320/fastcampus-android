a6956345116a461049928fa812f51ce1e7c32590
minji0320 <rlaalswl0320@naver.com>
2022-02-04 16:40:07 +0900

    Feat: 로또 번호 추첨기 - 07. Resource를 이용하여  꾸며보기

--- part2/ch02/app/src/main/java/fastcampus/aop/part2/chapter02/MainActivity.kt
+++ part2/ch02/app/src/main/java/fastcampus/aop/part2/chapter02/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -65,6 +66,8 @@ class MainActivity : AppCompatActivity() {
 
                 textView.isVisible = true
                 textView.text = number.toString()
+
+                setNumberBackground(number, textView)
             }
             Log.d("MainActivity", list.toString())
         }
@@ -91,7 +94,24 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value, textView)
+
             pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when (number) {
+            in 1..10 ->
+                textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 ->
+                textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 ->
+                textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 ->
+                textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else ->
+                textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
         }
     }
 
