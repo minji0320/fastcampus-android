1345692c668c58d40bac11a48b20c97943ab9231
minji0320 <rlaalswl0320@naver.com>
2022-02-08 20:37:06 +0900

    Feat: 뽀모도로 타이머 - 01. 기본 UI 구성하기

---
+++ part2/chapter06/app/src/main/java/fastcampus/aop/part2/chapter06/MainActivity.kt
@@ -0,0 +1,48 @@
+package fastcampus.aop.part2.chapter06
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean,
+                ) {
+                    remainMinutesTextView.text = "%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+        )
+    }
+}
