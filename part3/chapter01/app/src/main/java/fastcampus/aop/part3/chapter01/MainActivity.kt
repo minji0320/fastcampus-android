3fa8a48f1655233df3e0359921240ce04d1a4991
minji0320 <rlaalswl0320@naver.com>
2022-02-11 17:15:52 +0900

    Feat: 푸시 알림 수신기 - 01. 기본 UI 구성하기

---
+++ part3/chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -0,0 +1,21 @@
+package fastcampus.aop.part3.chapter01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseTokenTextView: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
