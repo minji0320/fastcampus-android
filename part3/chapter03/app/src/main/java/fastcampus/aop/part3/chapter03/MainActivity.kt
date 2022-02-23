947af1542f6078bc8753cb930a46dbf99da1fc71
minji0320 <rlaalswl0320@naver.com>
2022-02-24 00:17:42 +0900

    Feat: 알람앱 - 03. AlarmManager를 사용하여 알람 등록하기
    1. sharePreferences 값을 가져와서 화면 그리기
    2. PendingIntent를 활용하여 알람 정보 가져오기 (주석 처리)

--- part3/chapter03/app/src/main/java/fastcampus/aop/part3/chapter03/MainActivity.kt
+++ part3/chapter03/app/src/main/java/fastcampus/aop/part3/chapter03/MainActivity.kt
@@ -1,10 +1,13 @@
 package fastcampus.aop.part3.chapter03
 
+import android.app.PendingIntent
 import android.app.TimePickerDialog
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.TimePicker
 import java.util.*
 
@@ -22,13 +25,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // step 0 뷰를 초기화 해주기
         initOnOffButton()
         initChangeAlarmTimeButton()
 
-        // step 1 데이터 가져오기
-
-        // step 2 뷰에 데이터를 그려주기
+        val model = fetchDataFromSharedPreferences()
+        renderView(model)
     }
 
     private fun initOnOffButton() {
@@ -46,8 +47,8 @@ class MainActivity : AppCompatActivity() {
             val calendar = Calendar.getInstance()
             TimePickerDialog(this, { picker, hour, minute ->
                 val model = saveAlarmModel(hour, minute)
-                // 데이터 저장
-                // 뷰 업데이트
+                renderView(model)
+
                 // 기존 알람 삭제
             }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false)
                 .show()
@@ -56,12 +57,65 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveAlarmModel(hour: Int, minute: Int, onOff: Boolean = false): AlarmDisplayModel {
         val model = AlarmDisplayModel(hour, minute, onOff)
-        val sharePreferences = getSharedPreferences("time", Context.MODE_PRIVATE)
+        val sharePreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
         with(sharePreferences.edit()) {
-            putString("alarm", model.makeDataForDB())
-            putBoolean("onOff", model.onOff)
+            putString(ALARM_KEY, model.makeDataForDB())
+            putBoolean(ON_OFF_KEY, model.onOff)
             commit()
         }
         return model
+    }
+
+    private fun fetchDataFromSharedPreferences(): AlarmDisplayModel {
+        val sharePreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+        val timeDBValue = sharePreferences.getString(ALARM_KEY, "9:30") ?: "9:30"
+        val onOffDBValue = sharePreferences.getBoolean(ON_OFF_KEY, false)
+        val alarmData = timeDBValue.split(":")
+
+        val alarmModel = AlarmDisplayModel(
+            hour = alarmData[0].toInt(),
+            minute = alarmData[1].toInt(),
+            onOff = onOffDBValue
+        )
+
+//        val pendingIntent = PendingIntent.getBroadcast(
+//            this,
+//            ALARM_REQUEST_CODE,
+//            Intent(this, AlarmReceiver::class.java),
+//            PendingIntent.FLAG_NO_CREATE
+//        )
+//
+//        // 보정 (예외 처리)
+//        if ((pendingIntent == null) and alarmModel.onOff) {
+//            // 알람은 꺼져있는데, 저장된 데이터는 on인 경우
+//            alarmModel.onOff = false
+//        } else if ((pendingIntent != null) and !alarmModel.onOff) {
+//            // 알람은 켜져있는데, 저장된 데이터는 off인 경우 : 알람 취소
+//            pendingIntent.cancel()
+//        }
+
+        return alarmModel
+    }
+
+    private fun renderView(model: AlarmDisplayModel) {
+        findViewById<TextView>(R.id.ampmTextView).apply {
+            text = model.ampmText
+        }
+        findViewById<TextView>(R.id.timeTextView).apply {
+            text = model.timeText
+        }
+
+        onOffButton.apply {
+            text = model.onOffText
+            tag = model
+        }
+    }
+
+    companion object {
+        private const val SHARED_PREFERENCES_NAME = "time"
+        private const val ALARM_KEY = "alarm"
+        private const val ON_OFF_KEY = "onOff"
+        private const val ALARM_REQUEST_CODE = 1000
     }
 }
