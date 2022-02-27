ce94de807840422541642d784a78830476451081
minji0320 <rlaalswl0320@naver.com>
2022-02-28 00:35:27 +0900

    Feat: 도서 리뷰 앱 - 03. 도서 리스트 화면
    - RecyclerView 활용하여 아이템 그려보기

--- part3/chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
+++ part3/chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
@@ -3,7 +3,10 @@ package fastcampus.aop.part3.chapter04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.aop.part3.chapter04.adapter.BookAdapter
 import fastcampus.aop.part3.chapter04.api.BookService
+import fastcampus.aop.part3.chapter04.databinding.ActivityMainBinding
 import fastcampus.aop.part3.chapter04.model.BestCellerDto
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,9 +15,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBookRecyclerView()
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://book.interpark.com")
@@ -24,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         val bookService = retrofit.create(BookService::class.java)
 
         bookService.getBestSellerBooks("41C31BA633E8777B9142AE5A87718B2924E66955E911CDCFEAE1F833720465F6")
-            .enqueue(object: Callback<BestCellerDto> {
+            .enqueue(object : Callback<BestCellerDto> {
                 override fun onResponse(
                     call: Call<BestCellerDto>,
                     response: Response<BestCellerDto>,
@@ -39,6 +49,8 @@ class MainActivity : AppCompatActivity() {
                         it.books.forEach { book ->
                             Log.d(TAG, book.toString())
                         }
+
+                        adapter.submitList(it.books)
                     }
                 }
 
@@ -47,6 +59,13 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
+    }
+
+    private fun initBookRecyclerView() {
+        adapter = BookAdapter()
+
+        binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.bookRecyclerView.adapter = adapter
     }
 
     companion object {
