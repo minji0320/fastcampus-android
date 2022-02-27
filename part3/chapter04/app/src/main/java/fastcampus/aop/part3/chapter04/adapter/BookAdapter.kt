485fe87b8f7364ab6cf321657f26cdb0fec9aba5
minji0320 <rlaalswl0320@naver.com>
2022-02-28 00:50:52 +0900

    Feat: 도서 리뷰 앱 - 04. 도서 리스트 화면
    - 도서 목록 보여주기

--- part3/chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/adapter/BookAdapter.kt
+++ part3/chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/adapter/BookAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import fastcampus.aop.part3.chapter04.databinding.ItemBookBinding
 import fastcampus.aop.part3.chapter04.model.Book
 
@@ -15,6 +16,12 @@ class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) 
 
         fun bind(bookModel: Book) {
             binding.titleTextView.text = bookModel.title
+            binding.descriptionTextView.text = bookModel.description
+
+            Glide
+                .with(binding.coverImageView.context)
+                .load(bookModel.coverSmallUrl)
+                .into(binding.coverImageView)
         }
     }
 
