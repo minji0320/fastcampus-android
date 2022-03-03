1800f95d4a8b5d618ab2cc99db584d6a57a626fe
minji0320 <rlaalswl0320@naver.com>
2022-03-03 23:33:13 +0900

    Feat: 틴더 - 05. Firebase Realtime Database 연동하기

---
+++ part3/chapter05/app/src/main/java/fastcampus/aop/part3/chapter05/LikeActivity.kt
@@ -0,0 +1,80 @@
+package fastcampus.aop.part3.chapter05
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import fastcampus.aop.part3.chapter05.databinding.ActivityLikeBinding
+
+class LikeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLikeBinding
+    private var auth = FirebaseAuth.getInstance()
+    private lateinit var userDB: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLikeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userDB = Firebase.database.reference.child("Users")
+
+        val currentUserDB = userDB.child(getCurrentUserId())
+        currentUserDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child("name").value == null) {
+                    showNameInputPopup()
+                    return
+                }
+
+                // TODO 유저 정보 갱신
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    private fun showNameInputPopup() {
+        val editText = EditText(this)
+        AlertDialog.Builder(this)
+            .setTitle("이름을 입력해주세요")
+            .setView(editText)
+            .setPositiveButton("저장") { _, _ ->
+                if (editText.text.isEmpty()) {
+                    showNameInputPopup()
+                } else {
+                    saveUserName(editText.text.toString())
+                }
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun saveUserName(name: String) {
+        val userId = getCurrentUserId()
+        val currentUserDB = userDB.child(userId)
+        val user = mutableMapOf<String, Any>()
+        user["userId"] = userId
+        user["name"] = name
+        currentUserDB.updateChildren(user)
+    }
+
+    private fun getCurrentUserId(): String {
+        if (auth.currentUser == null) {
+            Toast.makeText(this, "로그인이 되어있지 않습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        return auth.currentUser?.uid.orEmpty()
+    }
+}
