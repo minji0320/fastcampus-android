052e2d9d51e2735f192eb5e3564ca227c5fb8be3
minji0320 <rlaalswl0320@naver.com>
2022-03-11 15:34:15 +0900

    Feat: 에어비엔비 - 02. 우리집 위치에 마커 찍어보기

--- part3/chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
+++ part3/chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
@@ -1,11 +1,18 @@
 package fastcampus.aop.part3.chapter07
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.naver.maps.map.MapView
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
     private val mapView: MapView by lazy {
         findViewById(R.id.mapView)
     }
@@ -13,6 +20,55 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        // Zoom 최대/최소 설정
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        // 맵 위치 변경 : 강남역
+        // scrollTo : 위도/경도로 설정
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.497879, 127.027604))
+        naverMap.moveCamera(cameraUpdate)
+
+        // 현위치 버튼 활성화
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+
+        // 위치를 가져오기 위해 권한이 필요함
+        locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+
+        // 마커 추가
+        val marker = Marker()
+        marker.position = LatLng(37.502264, 127.029635)
+        marker.map = naverMap
+        marker.icon = MarkerIcons.BLACK
+        marker.iconTintColor = Color.RED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
     }
 
     override fun onStart() {
@@ -48,5 +104,9 @@ class MainActivity : AppCompatActivity() {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
