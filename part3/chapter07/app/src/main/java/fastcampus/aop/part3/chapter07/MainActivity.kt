db44d10ff82c0d1f625a137249abb6b12b8796c7
minji0320 <rlaalswl0320@naver.com>
2022-03-11 18:02:55 +0900

    Feat: 에어비엔비 - 05. 지도 위에 예약가능 집 목록 띄우기

--- part3/chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
+++ part3/chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager2.widget.ViewPager2
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
@@ -20,12 +21,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         findViewById(R.id.mapView)
     }
 
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.houseViewPager)
+    }
+
+    private val viewPagerAdapter = HouseViewPagerAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView.onCreate(savedInstanceState)
 
         mapView.getMapAsync(this)
+
+        viewPager.adapter = viewPagerAdapter
     }
 
     override fun onMapReady(map: NaverMap) {
@@ -68,6 +77,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
                         response.body()?.let { dto ->
                             updateMarker(dto.items)
+                            viewPagerAdapter.submitList(dto.items)
                         }
                     }
 
