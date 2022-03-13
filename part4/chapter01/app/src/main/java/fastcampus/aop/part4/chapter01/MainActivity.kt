cbf9ccc943e2c189b3817d1f7f56808da9eb0be9
minji0320 <rlaalswl0320@naver.com>
2022-03-14 01:50:14 +0900

    Feat: 유튜브 - 02. MotionLayout 이용하여 화면 전환 UI 구성하기 (2)

--- part4/chapter01/app/src/main/java/fastcampus/aop/part4/chapter01/MainActivity.kt
+++ part4/chapter01/app/src/main/java/fastcampus/aop/part4/chapter01/MainActivity.kt
@@ -2,10 +2,19 @@ package fastcampus.aop.part4.chapter01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fastcampus.aop.part4.chapter01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlayerFragment())
+            .commit()
     }
 }
