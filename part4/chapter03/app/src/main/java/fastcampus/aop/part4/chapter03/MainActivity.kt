9a129197e8c3c37c59ee1ea892cf7a0a7b6fb672
minji0320 <rlaalswl0320@naver.com>
2022-03-20 00:11:48 +0900

    Feat: 위치검색 지도 앱 - 04. RecyclerView 구현하여 Mocking 데이터 뿌리기 - 4
    1. SearchRecyclerAdapter 구현

--- part4/chapter03/app/src/main/java/fastcampus/aop/part4/chapter03/MainActivity.kt
+++ part4/chapter03/app/src/main/java/fastcampus/aop/part4/chapter03/MainActivity.kt
@@ -2,6 +2,7 @@ package fastcampus.aop.part4.chapter03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import fastcampus.aop.part4.chapter03.databinding.ActivityMainBinding
@@ -22,7 +23,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initAdapter() {
-        adapter = SearchRecyclerAdapter()
+        adapter = SearchRecyclerAdapter {
+            Toast.makeText(this, "아이템 클릭", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
--- part4/chapter03/app/src/main/java/fastcampus/aop/part4/chapter03/SearchRecyclerAdapter.kt
+++ part4/chapter03/app/src/main/java/fastcampus/aop/part4/chapter03/SearchRecyclerAdapter.kt
@@ -1,33 +1,36 @@
 package fastcampus.aop.part4.chapter03
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import fastcampus.aop.part4.chapter03.databinding.ItemSearchResultBinding
 
-class SearchRecyclerAdapter : RecyclerView.Adapter<SearchRecyclerAdapter.ViewHolder>() {
+class SearchRecyclerAdapter(private val searchResultClickListener: (Any) -> Unit) :
+    RecyclerView.Adapter<SearchRecyclerAdapter.ViewHolder>() {
 
     private var searchResultList: List<Any> = listOf()
-    lateinit var searchResultClickListener: (Any) -> Unit
 
-    inner class ViewHolder(
-        val binding: ItemSearchResultBinding,
-        val searchResultClickListener: (Any) -> Unit,
-    ) :
+    inner class ViewHolder(val binding: ItemSearchResultBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(data: Any) = with(binding) {
             titleTextView.text = "제목"
             subTitleTextView.text = "부제목"
+
+            binding.root.setOnClickListener {
+                searchResultClickListener(data)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = ItemSearchResultBinding.bind(parent)
-        return ViewHolder(view, searchResultClickListener)
+        val view =
+            ItemSearchResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(searchResultList[position])
+        holder.bind(Any())
     }
 
     override fun getItemCount(): Int = 10
