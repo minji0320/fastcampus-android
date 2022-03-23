87c5d2319c26dd9bb0bfb933e86dd8ae00eb6ac9
minji0320 <rlaalswl0320@naver.com>
2022-03-23 23:03:24 +0900

    Feat: 깃헙 레포지토리 앱 - 02. 깃허브 연결 및 Auth 토큰 받아오기

--- part4/chapter05/app/src/main/java/fastcampus/aop/part4/chapter05/MainActivity.kt
+++ part4/chapter05/app/src/main/java/fastcampus/aop/part4/chapter05/MainActivity.kt
@@ -1,11 +1,105 @@
 package fastcampus.aop.part4.chapter05
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import fastcampus.aop.part4.chapter05.databinding.ActivityMainBinding
+import fastcampus.aop.part4.chapter05.utility.AuthTokenProvider
+import fastcampus.aop.part4.chapter05.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val authTokenProvider by lazy {
+        AuthTokenProvider(this)
+    }
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override
+
+    fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    private fun loginGithub() {
+        val loginUrl = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(this, loginUrl)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter("code")?.let {
+            launch(coroutineContext) {
+                showProgress()
+                val getAccessTokenJob = getAccessToken(it)
+                dismissProgress()
+            }
+        }
+    }
+
+    private suspend fun showProgress() = withContext(coroutineContext) {
+        with(binding) {
+            loginButton.isGone = true
+            progressBar.isGone = false
+            progressTextView.isGone = false
+        }
+    }
+
+    private suspend fun dismissProgress() = withContext(coroutineContext) {
+        with(binding) {
+            loginButton.isGone = false
+            progressBar.isGone = true
+            progressTextView.isGone = true
+        }
+    }
+
+    private suspend fun getAccessToken(code: String) = withContext(Dispatchers.IO) {
+        val response = RetrofitUtil.authApiService.getAccessToken(
+            clientId = BuildConfig.GITHUB_CLIENT_ID,
+            clientSecret = BuildConfig.GITHUB_CLIENT_SECRET,
+            code = code
+        )
+
+        if (response.isSuccessful) {
+            val accessToken = response.body()?.accessToken ?: ""
+            if (accessToken.isNotEmpty()) {
+                authTokenProvider.updateToken(accessToken)
+            } else {
+                Toast.makeText(this@MainActivity, "accessToken이 존재하지 않습니다.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
---
+++ part4/chapter05/app/src/main/java/fastcampus/aop/part4/chapter05/data/response/Url.kt
@@ -0,0 +1,9 @@
+package fastcampus.aop.part4.chapter05.data.response
+
+object Url {
+
+    const val GITHUB_URL = "https://github.com"
+
+    const val GITHUB_API_URL = "https://api.github.com"
+
+}
