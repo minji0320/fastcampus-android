d8cb6a7948d626083cd2761b28ced7157c0e0770
minji0320 <rlaalswl0320@naver.com>
2022-03-31 20:08:10 +0900

    Feat: 저작권 무료 이미지 검색기 - 02. 사진 정보 불러오기

---
+++ part4/chapter07/app/src/main/java/fastcampus/aop/part4/chapter07/data/UnsplashApiService.kt
@@ -0,0 +1,18 @@
+package fastcampus.aop.part4.chapter07.data
+
+import fastcampus.aop.part4.chapter07.BuildConfig
+import fastcampus.aop.part4.chapter07.data.models.PhotoResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UnsplashApiService {
+
+    @GET("photos/random?" +
+            "client_id=${BuildConfig.UNSPLASH_ACCESS_KEY}" +
+            "&count=30")
+    suspend fun getRandomPhoto(
+        @Query("query") query: String?
+    ): Response<List<PhotoResponse>>
+
+}
