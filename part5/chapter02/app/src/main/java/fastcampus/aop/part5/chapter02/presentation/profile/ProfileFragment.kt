96740e6fc9ba7dd872f688c45374b182aa6cb996
minji0320 <rlaalswl0320@naver.com>
2022-04-17 21:28:18 +0900

    Feat: 쇼핑 앱 만들기 - 07. 파이어베이스 로그인 기능 구현하기 - 2

--- part5/chapter02/app/src/main/java/fastcampus/aop/part5/chapter02/presentation/profile/ProfileFragment.kt
+++ part5/chapter02/app/src/main/java/fastcampus/aop/part5/chapter02/presentation/profile/ProfileFragment.kt
@@ -7,11 +7,13 @@ import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.Api
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import fastcampus.aop.part5.chapter02.R
 import fastcampus.aop.part5.chapter02.databinding.FragmentProfileBinding
+import fastcampus.aop.part5.chapter02.extensions.loadCenterCrop
+import fastcampus.aop.part5.chapter02.extensions.toast
 import fastcampus.aop.part5.chapter02.presentation.BaseFragment
 import org.koin.android.ext.android.inject
 
@@ -41,25 +43,29 @@ internal class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileB
                 try {
                     task.getResult(ApiException::class.java)?.let { account ->
                         Log.d(TAG, "firebaseAuthWithGoogle: ${account.id}")
-                        // TODO saveToken
+                        viewModel.saveToken(account.idToken ?: throw Exception())
                     } ?: throw Exception()
                 } catch (e: Exception) {
                     e.printStackTrace()
+                    handleErrorState()
                 }
+            } else {
+                handleErrorState()
             }
-
         }
 
     override fun getViewBinding(): FragmentProfileBinding =
         FragmentProfileBinding.inflate(layoutInflater)
 
-    override fun observeData() = viewModel.profileLiveData.observe(this) {
-        when (it) {
-            is ProfileState.Uninitialized -> initViews()
-            is ProfileState.Loading -> handleLoadingState()
-            is ProfileState.Login -> TODO()
-            is ProfileState.Success -> handleSuccessState(it)
-            is ProfileState.Error -> TODO()
+    override fun observeData() {
+        viewModel.profileLiveData.observe(this) {
+            when (it) {
+                is ProfileState.Uninitialized -> initViews()
+                is ProfileState.Loading -> handleLoadingState()
+                is ProfileState.Login -> handleLoginState(it)
+                is ProfileState.Success -> handleSuccessState(it)
+                is ProfileState.Error -> handleErrorState()
+            }
         }
     }
 
@@ -83,17 +89,49 @@ internal class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileB
         loginRequiredGroup.isGone = true
     }
 
+    private fun handleLoginState(state: ProfileState.Login) = with(binding) {
+        val credential = GoogleAuthProvider.getCredential(state.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    viewModel.setUserInfo(user)
+                } else {
+                    viewModel.setUserInfo(null)
+                }
+            }
+    }
+
     private fun handleSuccessState(state: ProfileState.Success) = with(binding) {
         progressBar.isGone = true
         when (state) {
             is ProfileState.Success.Registered -> {
-                // TODO Registered 구현
+                handleRegisteredState(state)
             }
             is ProfileState.Success.NotRegistered -> {
                 profileGroup.isGone = true
                 loginRequiredGroup.isVisible = true
             }
         }
+    }
+
+    private fun handleRegisteredState(state: ProfileState.Success.Registered) = with(binding) {
+        profileGroup.isVisible = true
+        loginRequiredGroup.isGone = true
+        profileImageView.loadCenterCrop(state.profileImageUri.toString(), 60f)
+        userNameTextView.text = state.userName
+
+        if (state.productList.isEmpty()) {
+            emptyResultTextView.isGone = false
+            recyclerView.isGone = true
+        } else {
+            emptyResultTextView.isGone = true
+            recyclerView.isGone = false
+        }
+    }
+
+    private fun handleErrorState() {
+        requireActivity().toast("에러가 발생했습니다.")
     }
 
 
--- part5/chapter02/app/src/main/java/fastcampus/aop/part5/chapter02/presentation/profile/ProfileViewModel.kt
+++ part5/chapter02/app/src/main/java/fastcampus/aop/part5/chapter02/presentation/profile/ProfileViewModel.kt
@@ -3,10 +3,13 @@ package fastcampus.aop.part5.chapter02.presentation.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseUser
 import fastcampus.aop.part5.chapter02.data.preference.PreferenceManager
 import fastcampus.aop.part5.chapter02.presentation.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 internal class ProfileViewModel(
     private val preferenceManager: PreferenceManager,
@@ -17,7 +20,7 @@ internal class ProfileViewModel(
 
     override fun fetchData(): Job = viewModelScope.launch {
         setState(ProfileState.Loading)
-        preferenceManager.getIdToken()?. let {
+        preferenceManager.getIdToken()?.let {
             setState(
                 ProfileState.Login(it)
             )
@@ -30,6 +33,27 @@ internal class ProfileViewModel(
 
     private fun setState(state: ProfileState) {
         _profileStateLiveData.postValue(state)
+    }
+
+    fun saveToken(idToken: String) = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            preferenceManager.putIdToken(idToken)
+            fetchData()
+        }
+    }
+
+    fun setUserInfo(firebaseUser: FirebaseUser?) = viewModelScope.launch {
+        firebaseUser?.let { user ->
+            setState(
+                ProfileState.Success.Registered(
+                    user.displayName ?: "익명",
+                    user.photoUrl,
+                    listOf()
+                )
+            )
+        } ?: kotlin.run {
+            setState(ProfileState.Success.NotRegistered)
+        }
     }
 
 }
