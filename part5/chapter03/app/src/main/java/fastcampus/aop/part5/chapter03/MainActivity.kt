a57cd44ea03bd7ae6940e826b45506a9c715446f
minji0320 <rlaalswl0320@naver.com>
2022-04-18 23:01:33 +0900

    Feat: 카메라 앱 - 02. CameraX 소개 및 기초 기능 구현 - 2

--- part5/chapter03/app/src/main/java/fastcampus/aop/part5/chapter03/MainActivity.kt
+++ part5/chapter03/app/src/main/java/fastcampus/aop/part5/chapter03/MainActivity.kt
@@ -1,14 +1,21 @@
 package fastcampus.aop.part5.chapter03
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.display.DisplayManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import fastcampus.aop.part5.chapter03.databinding.ActivityMainBinding
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +25,33 @@ class MainActivity : AppCompatActivity() {
     // 카메라 얻어오면 이후 실행 리스너 등록
     private val cameraProviderFuture by lazy {
         ProcessCameraProvider.getInstance(this)
+    }
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    private val cameraMainExecutor by lazy { ContextCompat.getMainExecutor(this) }
+
+    private lateinit var imageCapture: ImageCapture
+
+    private val displayManager by lazy {
+        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+
+    private var displayId: Int = -1
+
+    private var camera: Camera? = null
+
+    // 화면 전환 등의 이벤트 감지를 위해 필요
+    private val displayListener = object : DisplayManager.DisplayListener {
+        override fun onDisplayAdded(displayId: Int) = Unit
+
+        override fun onDisplayRemoved(displayId: Int) = Unit
+
+        override fun onDisplayChanged(displayId: Int) {
+            if (this@MainActivity.displayId == displayId) {
+
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +66,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera(viewFinder: PreviewView) {
+        displayManager.registerDisplayListener(displayListener, null)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        viewFinder.postDelayed({
+            displayId = viewFinder.display.displayId
+            bindCameraUseCase()
+        }, 10)
+    }
 
+    private fun bindCameraUseCase() = with(binding) {
+        val rotation = viewFinder.display.rotation
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(LENS_FACING).build()
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().apply {
+                setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                setTargetRotation(rotation)
+            }.build()
+
+            val imageCaptureBuilder = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(rotation)
+                .setFlashMode(ImageCapture.FLASH_MODE_AUTO)
+
+            imageCapture = imageCaptureBuilder.build()
+
+            try {
+                cameraProvider.unbindAll()
+                camera = cameraProvider.bindToLifecycle(
+                    this@MainActivity, cameraSelector, preview, imageCapture
+                )
+                preview.setSurfaceProvider(viewFinder.surfaceProvider)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, cameraMainExecutor)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                startCamera(binding.viewFinder)
+            } else {
+                Toast.makeText(this, "카메라 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
     private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
@@ -46,5 +124,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+        private const val LENS_FACING: Int = CameraSelector.LENS_FACING_BACK
     }
 }
