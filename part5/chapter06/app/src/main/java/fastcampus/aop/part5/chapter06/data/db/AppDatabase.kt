9a095bc0f631ad576cefebd1a766332abd8b2e8c
minji0320 <rlaalswl0320@naver.com>
2022-05-03 23:18:49 +0900

    Feat: 택배 조회 서비스 - 03. 택배 목록 불러오기

---
+++ part5/chapter06/app/src/main/java/fastcampus/aop/part5/chapter06/data/db/AppDatabase.kt
@@ -0,0 +1,24 @@
+package fastcampus.aop.part5.chapter06.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import fastcampus.aop.part5.chapter06.data.entity.TrackingItem
+
+@Database(
+    entities = [TrackingItem::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun TrackingItemDao(): TrackingItemDao
+
+    companion object {
+        private const val DATABASE_NAME = "tracking.db"
+
+        fun build(context: Context): AppDatabase =
+            Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME).build()
+    }
+}
---
+++ part5/chapter06/app/src/main/java/fastcampus/aop/part5/chapter06/data/entity/ShippingCompany.kt
@@ -0,0 +1,11 @@
+package fastcampus.aop.part5.chapter06.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ShippingCompany(
+    @PrimaryKey
+    val code: String,
+    val name: String,
+)
