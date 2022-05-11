1ec0a2e4f0e3f611adc3408405caec0d18dbf854
minji0320 <rlaalswl0320@naver.com>
2022-05-11 22:30:37 +0900

    Feat: 영화 평점 서비스 - 09. 리뷰 작성 및 삭제 UseCase 작성하기

--- part5/chapter07/app/src/main/java/fastcampus/aop/part5/chapter07/data/api/ReviewFirestoreApi.kt
+++ part5/chapter07/app/src/main/java/fastcampus/aop/part5/chapter07/data/api/ReviewFirestoreApi.kt
@@ -2,12 +2,14 @@ package fastcampus.aop.part5.chapter07.data.api
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.toObject
+import fastcampus.aop.part5.chapter07.domain.model.Movie
 import fastcampus.aop.part5.chapter07.domain.model.Review
 import kotlinx.coroutines.tasks.await
 
 class ReviewFirestoreApi(
-    private val firestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore,
 ) : ReviewApi {
 
     override suspend fun getLatestReview(movieId: String): Review? =
@@ -35,4 +37,68 @@ class ReviewFirestoreApi(
             .get()
             .await()
             .map { it.toObject() }
+
+    override suspend fun addReview(review: Review): Review {
+        val newReviewPreference = firestore.collection("reviews").document()
+        val moviePreference = firestore.collection("movies").document(review.movieId!!)
+
+        firestore.runTransaction { transition ->
+            val movie = transition.get(moviePreference).toObject<Movie>()!!
+
+            val oldAverageScore = movie.averageScore ?: 0f
+            val oldNumberOfScore = movie.numberOfScore ?: 0
+            val oldTotalScore = oldAverageScore * oldNumberOfScore
+
+            val newNumberOfScore = oldNumberOfScore + 1
+            val newAverageScore = (oldTotalScore + (review.score ?: 0f)) / newNumberOfScore
+
+            transition.set(
+                moviePreference,
+                movie.copy(
+                    numberOfScore = newNumberOfScore,
+                    averageScore = newAverageScore
+                )
+            )
+
+            transition.set(
+                newReviewPreference,
+                review,
+                SetOptions.merge()
+            )
+        }.await()
+
+        return newReviewPreference.get().await().toObject<Review>()!!
+    }
+
+    override suspend fun removeReview(review: Review) {
+        val reviewReference = firestore.collection("reviews").document(review.id!!)
+        val movieReference = firestore.collection("movies").document(review.movieId!!)
+
+        firestore.runTransaction { transaction ->
+            val movie = transaction
+                .get(movieReference)
+                .toObject<Movie>()!!
+
+            val oldAverageScore = movie.averageScore ?: 0f
+            val oldNumberOfScore = movie.numberOfScore ?: 0
+            val oldTotalScore = oldAverageScore * oldNumberOfScore
+
+            val newNumberOfScore = (oldNumberOfScore - 1).coerceAtLeast(0)
+            val newAverageScore = if (newNumberOfScore > 0) {
+                (oldTotalScore - (review.score ?: 0f)) / newNumberOfScore
+            } else {
+                0f
+            }
+
+            transaction.set(
+                movieReference,
+                movie.copy(
+                    numberOfScore = newNumberOfScore,
+                    averageScore = newAverageScore
+                )
+            )
+
+            transaction.delete(reviewReference)
+        }.await()
+    }
 }
---
+++ part5/chapter07/app/src/main/java/fastcampus/aop/part5/chapter07/domain/usecase/DeleteReviewUseCase.kt
@@ -0,0 +1,10 @@
+package fastcampus.aop.part5.chapter07.domain.usecase
+
+import fastcampus.aop.part5.chapter07.data.repository.ReviewRepository
+import fastcampus.aop.part5.chapter07.domain.model.Review
+
+class DeleteReviewUseCase(private val reviewRepository: ReviewRepository) {
+
+    suspend operator fun invoke(review: Review) =
+        reviewRepository.removeReview(review)
+}
