0f9728cdbb259c0542a3d53ceb39c3f4c33eb96d
minji0320 <rlaalswl0320@naver.com>
2022-05-11 17:50:13 +0900

    Feat: 영화 평점 서비스 - 07. 내가 리뷰한 영화 목록 불러오기

--- part5/chapter07/app/src/main/java/fastcampus/aop/part5/chapter07/di/AppModule.kt
+++ part5/chapter07/app/src/main/java/fastcampus/aop/part5/chapter07/di/AppModule.kt
@@ -1,12 +1,16 @@
 package fastcampus.aop.part5.chapter07.di
 
+import android.app.Activity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import fastcampus.aop.part5.chapter07.data.api.*
+import fastcampus.aop.part5.chapter07.data.preference.PreferenceManager
+import fastcampus.aop.part5.chapter07.data.preference.SharedPreferenceManager
 import fastcampus.aop.part5.chapter07.data.repository.*
 import fastcampus.aop.part5.chapter07.domain.model.Movie
 import fastcampus.aop.part5.chapter07.domain.usecase.GetAllMoviesUseCase
 import fastcampus.aop.part5.chapter07.domain.usecase.GetAllMovieReviewsUseCase
+import fastcampus.aop.part5.chapter07.domain.usecase.GetMyReviewedMoviesUseCase
 import fastcampus.aop.part5.chapter07.domain.usecase.GetRandomFeaturedMovieUseCase
 import fastcampus.aop.part5.chapter07.presentation.home.HomeContract
 import fastcampus.aop.part5.chapter07.presentation.home.HomeFragment
@@ -27,15 +31,21 @@ val dataModule = module {
 
     single<MovieApi> { MovieFirestoreApi(get()) }
     single<ReviewApi> { ReviewFirestoreApi(get()) }
+    single<UserApi> { UserFirestoreApi(get()) }
 
     single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
     single<ReviewRepository> { ReviewRepositoryImpl(get(), get()) }
+    single<UserRepository> { UserRepositoryImpl(get(), get(), get()) }
+
+    single { androidContext().getSharedPreferences("preference", Activity.MODE_PRIVATE) }
+    single<PreferenceManager> { SharedPreferenceManager(get()) }
 }
 
 val domainModule = module {
     factory { GetAllMoviesUseCase(get()) }
     factory { GetAllMovieReviewsUseCase(get()) }
     factory { GetRandomFeaturedMovieUseCase(get(), get()) }
+    factory { GetMyReviewedMoviesUseCase(get(), get(), get()) }
 }
 
 val presentationModule = module {
