a89b65467c404932174dae693b3e7cb68729abff
minji0320 <rlaalswl0320@naver.com>
2022-05-26 23:48:43 +0900

    Feat: 배달앱 - 26. 구글 로그인 연동 구현하기 1

--- part6/chapter01/app/src/main/java/fastcampus/aop/part6/chapter01/screen/main/my/MyFragment.kt
+++ part6/chapter01/app/src/main/java/fastcampus/aop/part6/chapter01/screen/main/my/MyFragment.kt
@@ -1,9 +1,14 @@
 package fastcampus.aop.part6.chapter01.screen.main.my
 
-import fastcampus.aop.part6.chapter01.databinding.FragmentHomeBinding
+import android.app.Activity
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import fastcampus.aop.part6.chapter01.R
 import fastcampus.aop.part6.chapter01.databinding.FragmentMyBinding
 import fastcampus.aop.part6.chapter01.screen.base.BaseFragment
-import fastcampus.aop.part6.chapter01.screen.main.home.HomeFragment
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MyFragment : BaseFragment<MyViewModel, FragmentMyBinding>() {
@@ -11,6 +16,42 @@ class MyFragment : BaseFragment<MyViewModel, FragmentMyBinding>() {
 
     override fun getViewBinding(): FragmentMyBinding =
         FragmentMyBinding.inflate(layoutInflater)
+
+    private val gso: GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+    }
+
+    private val gsc by lazy { GoogleSignIn.getClient(requireActivity(), gso) }
+
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    private val loginLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                try {
+                    task.getResult(ApiException::class.java)?.let { account ->
+                        viewModel.saveToken(account.idToken ?: throw Exception())
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+    override fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = gsc.signInIntent
+        loginLauncher.launch(signInIntent)
+    }
 
     override fun observeData() {
     }
--- part6/chapter01/app/src/main/java/fastcampus/aop/part6/chapter01/screen/main/my/MyViewModel.kt
+++ part6/chapter01/app/src/main/java/fastcampus/aop/part6/chapter01/screen/main/my/MyViewModel.kt
@@ -3,4 +3,7 @@ package fastcampus.aop.part6.chapter01.screen.main.my
 import fastcampus.aop.part6.chapter01.screen.base.BaseViewModel
 
 class MyViewModel: BaseViewModel() {
+    fun saveToken(idToken: String) {
+
+    }
 }
