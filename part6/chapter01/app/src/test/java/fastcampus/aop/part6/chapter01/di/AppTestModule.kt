55783215833cbdc6e30b703c81a696da8ca5f1ea
minji0320 <rlaalswl0320@naver.com>
2022-06-07 22:00:43 +0900

    Feat: 배달앱 - 39. 테스트 코드 - 식당 리스트

--- part6/chapter01/app/src/test/java/fastcampus/aop/part6/chapter01/di/AppTestModule.kt
+++ part6/chapter01/app/src/test/java/fastcampus/aop/part6/chapter01/di/AppTestModule.kt
@@ -1,7 +1,18 @@
 package fastcampus.aop.part6.chapter01.di
 
+import fastcampus.aop.part6.chapter01.data.TestRestaurantRepository
+import fastcampus.aop.part6.chapter01.data.entity.LocationLatLngEntity
+import fastcampus.aop.part6.chapter01.data.repository.restaurant.RestaurantRepository
+import fastcampus.aop.part6.chapter01.screen.main.home.restaurant.RestaurantCategory
+import fastcampus.aop.part6.chapter01.screen.main.home.restaurant.RestaurantListViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 internal val appTestModule = module {
 
+    viewModel { (restaurantCategory: RestaurantCategory, locationLatLng: LocationLatLngEntity) ->
+        RestaurantListViewModel(restaurantCategory, locationLatLng, get())
+    }
+
+    single<RestaurantRepository> { TestRestaurantRepository() }
 }
---
+++ part6/chapter01/app/src/test/java/fastcampus/aop/part6/chapter01/viewmodel/restaurant/RestaurantListViewModelTest.kt
@@ -0,0 +1,105 @@
+package fastcampus.aop.part6.chapter01.viewmodel.restaurant
+
+import fastcampus.aop.part6.chapter01.data.entity.LocationLatLngEntity
+import fastcampus.aop.part6.chapter01.data.repository.restaurant.RestaurantRepository
+import fastcampus.aop.part6.chapter01.model.restaurant.RestaurantModel
+import fastcampus.aop.part6.chapter01.screen.main.home.restaurant.RestaurantCategory
+import fastcampus.aop.part6.chapter01.screen.main.home.restaurant.RestaurantListViewModel
+import fastcampus.aop.part6.chapter01.screen.main.home.restaurant.RestaurantOrder
+import fastcampus.aop.part6.chapter01.viewmodel.ViewModelTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.koin.core.parameter.parametersOf
+import org.koin.test.inject
+
+@ExperimentalCoroutinesApi
+internal class RestaurantListViewModelTest : ViewModelTest() {
+
+    /**
+     * [RestaurantCategory]
+     * [LocationLatLngEntity]
+     */
+
+    private var restaurantCategory = RestaurantCategory.ALL
+
+    private val locationLatLngEntity: LocationLatLngEntity = LocationLatLngEntity(0.0, 0.0)
+
+    private val restaurantRepository by inject<RestaurantRepository>()
+
+    private val restaurantListViewModel by inject<RestaurantListViewModel> {
+        parametersOf(
+            restaurantCategory,
+            locationLatLngEntity
+        )
+    }
+
+    @Test
+    fun `test load restaurant list category ALL`() = runBlockingTest {
+        val testObservable = restaurantListViewModel.restaurantListLiveData.test()
+
+        restaurantListViewModel.fetchData()
+
+        testObservable.assertValueSequence(
+            listOf(
+                restaurantRepository.getList(restaurantCategory, locationLatLngEntity).map {
+                    RestaurantModel(
+                        id = it.id,
+                        restaurantInfoId = it.restaurantInfoId,
+                        restaurantCategory = it.restaurantCategory,
+                        restaurantTitle = it.restaurantTitle,
+                        restaurantImageUrl = it.restaurantImageUrl,
+                        grade = it.grade,
+                        reviewCount = it.reviewCount,
+                        deliveryTimeRange = it.deliveryTimeRange,
+                        deliveryTipRange = it.deliveryTipRange,
+                        restaurantTelNumber = it.restaurantTelNumber
+                    )
+                }
+            )
+        )
+    }
+
+    @Test
+    fun `test load restaurant list category Excepted`() = runBlockingTest {
+        restaurantCategory = RestaurantCategory.CAFE_DESSERT
+
+        val testObservable = restaurantListViewModel.restaurantListLiveData.test()
+
+        restaurantListViewModel.fetchData()
+
+        testObservable.assertValueSequence(
+            listOf(
+                listOf()
+            )
+        )
+    }
+
+    @Test
+    fun `test load restaurant list order by fast delivery time`() = runBlockingTest {
+        val testObservable = restaurantListViewModel.restaurantListLiveData.test()
+
+        restaurantListViewModel.setRestaurantOrder(RestaurantOrder.FAST_DELIVERY)
+
+        testObservable.assertValueSequence(
+            listOf(
+                restaurantRepository.getList(restaurantCategory, locationLatLngEntity)
+                    .sortedBy { it.deliveryTimeRange.first }
+                    .map {
+                        RestaurantModel(
+                            id = it.id,
+                            restaurantInfoId = it.restaurantInfoId,
+                            restaurantCategory = it.restaurantCategory,
+                            restaurantTitle = it.restaurantTitle,
+                            restaurantImageUrl = it.restaurantImageUrl,
+                            grade = it.grade,
+                            reviewCount = it.reviewCount,
+                            deliveryTimeRange = it.deliveryTimeRange,
+                            deliveryTipRange = it.deliveryTipRange,
+                            restaurantTelNumber = it.restaurantTelNumber
+                        )
+                    }
+            )
+        )
+    }
+}
